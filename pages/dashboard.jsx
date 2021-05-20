a6d1378c921fec1e7009866f433d1c613bfb840d
prateeksoni1 <proforty7@gmail.com>
2021-05-20 13:30:55 +0530

    add order
    Signed-off-by: prateeksoni1 <proforty7@gmail.com>

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { privateApi } from "../api";
@@ -22,66 +23,68 @@ export default function DashboardPage(props) {
 
   return (
     <PrivateHOC user={props.user}>
-      <div className='container'>
+      <div className='container' style={{ minHeight: "80vh" }}>
         <div className='row'>
-          <div
-            className='card col-8'
-            style={{ width: 500, marginTop: 130, marginLeft: 20 }}
-          >
-            <div className='card-body'>
-              <form
-                className='form-control-sm'
-                onSubmit={handleSubmit(onSubmit)}
-              >
-                <div className='mb-3'>
-                  <div class='input-group mb-3'>
-                    <div class='input-group mb-3'>
-                      <span class='input-group-text' id='basic-addon3'>
-                        Dripfeed (Number of days)
-                      </span>
-                      <input
-                        type='number'
-                        class='form-control'
-                        id='basic-url'
-                        aria-describedby='basic-addon3'
-                        placeholder='        Range of 1 to 30'
-                        {...register("dripfeed", {
-                          required: true,
-                          max: 30,
-                        })}
-                      />
-
-                      {errors?.dripfeed && (
-                        <span className='text-danger'>
-                          Must be between 1 and 30
+          <div className='col-7'>
+            <div
+              className='card w-100'
+              style={{ width: 500, marginTop: 130, marginLeft: 20 }}
+            >
+              <div className='card-body'>
+                <form
+                  className='form-control-sm'
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <div className='mb-3'>
+                    <div className='input-group mb-3'>
+                      <div className='input-group mb-3'>
+                        <span className='input-group-text' id='basic-addon3'>
+                          Dripfeed (Number of days)
                         </span>
-                      )}
+                        <input
+                          type='number'
+                          className='form-control'
+                          id='basic-url'
+                          aria-describedby='basic-addon3'
+                          placeholder='        Range of 1 to 30'
+                          {...register("dripfeed", {
+                            required: true,
+                            max: 30,
+                          })}
+                        />
+
+                        {errors?.dripfeed && (
+                          <span className='text-danger'>
+                            Must be between 1 and 30
+                          </span>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className='mb-3'>
-                  <div class='input-group'>
-                    <span class='input-group-text'>Paste Links</span>
-                    <textarea
-                      class='form-control'
-                      aria-label='With textarea'
-                      {...register("links", {
-                        required: true,
-                      })}
-                    ></textarea>
+                  <div className='mb-3'>
+                    <div className='input-group'>
+                      <span className='input-group-text'>Paste Links</span>
+                      <textarea
+                        className='form-control'
+                        aria-label='With textarea'
+                        {...register("links", {
+                          required: true,
+                        })}
+                      ></textarea>
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <button type='submit' className='btn btn-primary w-100'>
-                    Add Links
-                  </button>
-                </div>
-              </form>
+                  <div>
+                    <button type='submit' className='btn btn-primary btn-lg'>
+                      Add Links
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-          <div className='col-4 container'>
-            <div className='justify-content-center'>
+          <div className='d-flex col-5 align-items-center justify-content-end'>
+            <div>
               <h1>Your Account</h1>
               <ul>
                 <li>
