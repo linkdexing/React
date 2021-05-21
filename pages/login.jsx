ab647e8cc2f68db4c1011cb50dc364efd7e1ce39
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-05-21 20:57:30 +0530

    minor changes
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- pages/login.jsx
+++ pages/login.jsx
@@ -28,25 +28,25 @@ export default function login(props) {
 
   return (
     <PublicHOC user={props.user}>
-      <div className='container'>
-        <div className='row'>
+      <div className="container">
+        <div className="row">
           <div
-            className='card col-8'
+            className="card col-8"
             style={{ width: 500, marginTop: 130, marginLeft: 20 }}
           >
-            <div className='card-body'>
+            <div className="card-body">
               <form
-                className='form-control-sm'
+                className="form-control-sm"
                 onSubmit={handleSubmit(onSubmit)}
               >
-                <div className='mb-3'>
-                  <label htmlFor='exampleInputEmail1' className='form-label'>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
                     Email address
                   </label>
                   <input
-                    type='email'
-                    className='form-control'
-                    aria-describedby='emailHelp'
+                    type="email"
+                    className="form-control"
+                    aria-describedby="emailHelp"
                     {...register("email", {
                       required: true,
                       pattern:
@@ -54,37 +54,37 @@ export default function login(props) {
                     })}
                   />
                   {errors?.email && (
-                    <span className='text-danger'>Incorrect Email.</span>
+                    <span className="text-danger">Incorrect Email.</span>
                   )}
                 </div>
-                <div className='mb-3'>
-                  <label htmlFor='exampleInputPassword1' className='form-label'>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
                     Password
                   </label>
                   <input
-                    type='password'
-                    className='form-control'
+                    type="password"
+                    className="form-control"
                     {...register("password", {
                       required: true,
                     })}
                   />
                   {errors?.password && (
-                    <span className='text-danger'>Incorrect Password.</span>
+                    <span className="text-danger">Incorrect Password.</span>
                   )}
                 </div>
 
                 <div>
-                  <Link href='/register'>
+                  <Link href="/register">
                     <a>Forgot Password?</a>
                   </Link>
-                  <button type='submit' className='btn btn-primary w-100'>
+                  <button type="submit" className="btn btn-primary w-100">
                     Login
                   </button>
                 </div>
                 <p></p>
                 <div>
                   <span>Not a member? </span>
-                  <Link href='/register'>
+                  <Link href="/register">
                     <a>Register Now</a>
                   </Link>
                 </div>
