c2eaf244461b66608a6cf28fcaedd46d5f6ce7c7
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-05-26 23:47:02 +0530

    minor change in register
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- pages/register.jsx
+++ pages/register.jsx
@@ -3,8 +3,11 @@ import { useForm } from "react-hook-form";
 import { publicApi } from "../api";
 import { authUrl } from "../api/endpoints";
 import PublicHOC from "../components/publicHOC";
+import { useRouter } from "next/router";
 
 export default function RegisterPage(props) {
+  const router = useRouter();
+
   const {
     register,
     handleSubmit,
@@ -13,37 +16,38 @@ export default function RegisterPage(props) {
 
   const onSubmit = async (values) => {
     const res = await publicApi.post(authUrl, values);
+    router.push("/login");
     console.log(res);
   };
 
   return (
     <PublicHOC user={props.user}>
-      <div className='container'>
-        <div className='card' style={{ width: 500, marginTop: "6rem" }}>
-          <div className='card-body'>
-            <form className='form-control-sm' onSubmit={handleSubmit(onSubmit)}>
-              <div className='mb-3'>
-                <label for='exampleInputName' className='form-label'>
+      <div className="container">
+        <div className="card" style={{ width: 500, marginTop: "6rem" }}>
+          <div className="card-body">
+            <form className="form-control-sm" onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-3">
+                <label for="exampleInputName" className="form-label">
                   Name
                 </label>
                 <input
-                  type='text'
-                  className='form-control'
-                  name='name'
+                  type="text"
+                  className="form-control"
+                  name="name"
                   {...register("name", { required: true, minLength: 4 })}
                 />
                 {errors?.name && (
-                  <span className='text-danger'>Name is too short</span>
+                  <span className="text-danger">Name is too short</span>
                 )}
               </div>
-              <div className='mb-3'>
-                <label for='exampleInputEmail1' className='form-label'>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">
                   Email address
                 </label>
                 <input
-                  type='email'
-                  className='form-control'
-                  aria-describedby='emailHelp'
+                  type="email"
+                  className="form-control"
+                  aria-describedby="emailHelp"
                   {...register("email", {
                     required: true,
                     pattern:
@@ -51,33 +55,33 @@ export default function RegisterPage(props) {
                   })}
                 />
                 {errors?.email && (
-                  <span className='text-danger'>Invalid Email format</span>
+                  <span className="text-danger">Invalid Email format</span>
                 )}
               </div>
-              <div className='mb-3'>
-                <label for='exampleInputPassword1' className='form-label'>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
                   Password
                 </label>
                 <input
-                  type='password'
-                  className='form-control'
+                  type="password"
+                  className="form-control"
                   {...register("password", {
                     required: true,
                     pattern: /^.{5,}$/,
                   })}
-                  aria-describedby='passwordlHelp'
+                  aria-describedby="passwordlHelp"
                 />
                 {errors?.password && (
-                  <span className='text-danger'>
+                  <span className="text-danger">
                     Password must be atleast 5 characters long
                   </span>
                 )}
               </div>
-              <button type='submit' className='btn btn-primary w-100'>
+              <button type="submit" className="btn btn-primary w-100">
                 Register
               </button>
-              <div className='mt-2'>
-                <Link href='/login'>
+              <div className="mt-2">
+                <Link href="/login">
                   <a>Already registered? Login</a>
                 </Link>
               </div>
