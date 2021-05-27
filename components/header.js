1cd703e00569ad0c5bed212b22c5e5e049120c6b
prateeksoni1 <proforty7@gmail.com>
2021-05-27 22:21:59 +0530

    fix login bug
    Signed-off-by: prateeksoni1 <proforty7@gmail.com>

--- components/header.js
+++ components/header.js
@@ -5,72 +5,77 @@ import { useRouter } from "next/router";
 export default function Header(props) {
   const router = useRouter();
 
-  const onLogout = useCallback(() => {
+  const onLogout = () => {
     localStorage.removeItem("linkdexing_token");
+    props.setUser(null);
     router.replace("/");
-  }, []);
+  };
 
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-light bg-light"
+      className='navbar navbar-expand-lg navbar-light bg-light'
       style={{ minHeight: 80, fontSize: 20 }}
     >
-      <div className="container">
+      <div className='container'>
         <span>
           <a
-            className="container navbar-brand d-flex"
-            href="/"
+            className='container navbar-brand d-flex'
+            href='/'
             style={{ fontSize: "2rem" }}
           >
             Linkdexing.com
           </a>
         </span>
         <button
-          className="navbar-toggler justify-content-end"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler justify-content-end'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarTogglerDemo01'
+          aria-controls='navbarTogglerDemo01'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div className='collapse navbar-collapse' id='navbarTogglerDemo01'>
           {/* <a className="navbar-brand" href="/" style={{ fontSize: "2rem" }}>
             Linkdexing.com
           </a> */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
                 About Us
               </a>
             </li>
           </ul>
-          <form className="d-flex">
+          <>
             {props.user ? (
               <>
                 <div
-                  className="nav-link text-dark"
+                  className='nav-link text-dark'
                   style={{ marginRight: "1rem" }}
                 >
                   {props.user.email}
                 </div>
-                <button className="btn btn-outline-danger" onClick={onLogout}>
+                <button
+                  type='button'
+                  className='btn btn-outline-danger'
+                  onClick={onLogout}
+                >
                   Logout
                 </button>
               </>
             ) : (
-              <Link href="/login">
-                <a className="btn btn-outline-success">Login</a>
+              <Link href='/login'>
+                <a className='btn btn-outline-success'>Login</a>
               </Link>
             )}
-          </form>
+          </>
         </div>
       </div>
     </nav>
