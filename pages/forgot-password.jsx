38bc84b8174a015432246c2071600b2a73ef3dc9
prateeksoni1 <proforty7@gmail.com>
2021-06-08 15:05:13 +0530

    add forgot password
    Signed-off-by: prateeksoni1 <proforty7@gmail.com>

---
+++ pages/forgot-password.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { publicApi } from "../api";
+import { authUrl } from "../api/endpoints";
+
+const ForgotPasswordPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (values) => {
+    try {
+      await publicApi.post(`${authUrl}/forgot-password`, values);
+      toast.success("Password reset email sent");
+    } catch (err) {
+      toast.error(err.message || err.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form className='container mt-4' onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-3'>
+          <label for='email' className='form-label'>
+            Email Address
+          </label>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            {...register("email", {
+              required: "Email address is required",
+              pattern: {
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          {errors?.email && (
+            <span className='text-danger'>{errors.email.message}</span>
+          )}
+        </div>
+
+        <button type='submit' className='btn btn-primary'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ pages/reset-password.jsx
@@ -0,0 +1,85 @@
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { publicApi } from "../api";
+import { authUrl } from "../api/endpoints";
+
+const ResetPasswordPage = () => {
+  const router = useRouter();
+
+  const { id, token } = router.query;
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const watchNewPassword = watch("newPassword");
+
+  const onSubmit = async (values) => {
+    try {
+      await publicApi.post(`${authUrl}/reset-password`, {
+        ...values,
+        id,
+        token,
+      });
+      toast.success("Password changed successfully. Please login to continue");
+      router.replace("/login");
+    } catch (err) {
+      toast.error(err.message || err.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form className='container mt-4' onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-3'>
+          <label for='newPassword' className='form-label'>
+            New Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            id='newPassword'
+            {...register("newPassword", {
+              required: "New password is required",
+              minLength: {
+                value: 5,
+                message: "Your password is too short",
+              },
+            })}
+          />
+          {errors?.newPassword && (
+            <span className='text-danger'>{errors.newPassword.message}</span>
+          )}
+        </div>
+        <div className='mb-3'>
+          <label for='newPasswordConfirm' className='form-label'>
+            Confirm New Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            id='newPasswordConfirm'
+            {...register("newPasswordConfirm", {
+              validate: (value) =>
+                value === watchNewPassword || "The passwords do not match",
+            })}
+          />
+          {errors?.newPasswordConfirm && (
+            <span className='text-danger'>
+              {errors.newPasswordConfirm.message}
+            </span>
+          )}
+        </div>
+        <button type='submit' className='btn btn-primary'>
+          Reset Password
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
