e20fa7209f8c3d183f4454a9d18592d1be9e7190
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-06-18 22:41:52 +0530

    Added confirm password in registration
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -15,7 +15,10 @@ export default function RegisterPage({ setRefresh, user }) {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm();
+
+  const watchPassword = watch("password");
 
   const handleRecaptchaVerify = useCallback(async (token) => {
     try {
@@ -68,35 +71,35 @@ export default function RegisterPage({ setRefresh, user }) {
   }, [executeRecaptcha]);
 
   return (
-    <div className='container'>
-      <div className='card' style={{ width: 500, marginTop: "6rem" }}>
-        <div className='card-body'>
-          <form className='form-control-sm' onSubmit={handleSubmit(onSubmit)}>
-            <div className='mb-3'>
-              <label htmlFor='name' className='form-label'>
+    <div className="container">
+      <div className="card" style={{ width: 500, marginTop: "6rem" }}>
+        <div className="card-body">
+          <form className="form-control-sm" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
                 Name
               </label>
               <input
-                type='text'
-                id='name'
-                className='form-control'
+                type="text"
+                id="name"
+                className="form-control"
                 {...register("name", { required: true, minLength: 4 })}
               />
               {errors?.name && (
-                <span className='text-danger'>
+                <span className="text-danger">
                   Name should be atleast 5 characters long
                 </span>
               )}
             </div>
-            <div className='mb-3'>
-              <label htmlFor='email' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
                 Email address
               </label>
               <input
-                type='email'
-                id='email'
-                className='form-control'
-                aria-describedby='emailHelp'
+                type="email"
+                id="email"
+                className="form-control"
+                aria-describedby="emailHelp"
                 {...register("email", {
                   required: true,
                   pattern:
@@ -104,37 +107,60 @@ export default function RegisterPage({ setRefresh, user }) {
                 })}
               />
               {errors?.email && (
-                <span className='text-danger'>Invalid Email format</span>
+                <span className="text-danger">Invalid Email format</span>
               )}
             </div>
-            <div className='mb-3'>
-              <label htmlFor='password' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
                 Password
               </label>
               <input
-                type='password'
-                id='password'
-                className='form-control'
+                type="password"
+                id="password"
+                className="form-control"
                 {...register("password", {
                   required: true,
                   pattern: /^.{5,}$/,
                 })}
-                aria-describedby='passwordlHelp'
+                aria-describedby="passwordlHelp"
               />
               {errors?.password && (
-                <span className='text-danger'>
+                <span className="text-danger">
                   Password must be atleast 5 characters long
+                </span>
+              )}
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="passwordConfirm" className="form-label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="passwordConfirm"
+                aria-describedby="Confirm Password"
+                {...register("passwordConfirm", {
+                  required: "Confirm password is required",
+                  validate: (value) =>
+                    value === watchPassword || "The passwords do not match",
+                })}
+              />
+
+              {errors?.passwordConfirm && (
+                <span className="text-danger">
+                  {errors.passwordConfirm.message}
                 </span>
               )}
             </div>
 
             <GoogleReCaptcha onVerify={handleRecaptchaVerify} />
 
-            <button type='submit' className='btn btn-primary w-100'>
+            <button type="submit" className="btn btn-primary w-100">
               Register
             </button>
-            <div className='mt-2'>
-              <Link to='/login'>Already registered? Login</Link>
+            <div className="mt-2">
+              <Link to="/login">Already registered? Login</Link>
             </div>
           </form>
         </div>
